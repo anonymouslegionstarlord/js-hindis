07c57e960ec6ddcfd8d64aa8c5d913025f4d6dd3
AnonymousLegion <53853011+anonymouslegionstarlord@users.noreply.github.com>
2024-07-24 04:18:59 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat as newer version of js
+
+// alert (3+3) // we are using node js not browser
+
+console.log(3+3); // code readability should be high
+console.log("Mayank")
+
+let name ="mayank"
+let age =17
+let isLoggedIn= false
+let state ;
+
+
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
