ac8b349af11881760125add50c8430a3973dc908
AnonymousLegion <53853011+anonymouslegionstarlord@users.noreply.github.com>
2024-08-21 10:44:26 +0000

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,16 @@
+const user={
+    username: "MAyank",
+    price:999,
+    
+    welcomeMessage :function () {
+        console.log(`${this.username}, welcome to site`);
+        
+        
+    }
+}
+
+// user.welcomeMessage()
+// user.username="Rock"
+// user.welcomeMessage()
+
+log
