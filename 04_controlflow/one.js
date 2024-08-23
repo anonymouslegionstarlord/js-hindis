e6d38d45e193693c40518bf081e3092d90effe21
AnonymousLegion <53853011+anonymouslegionstarlord@users.noreply.github.com>
2024-08-23 20:49:26 +0000

    switch

---
+++ 04_controlflow/one.js
@@ -0,0 +1,63 @@
+// //  if
+// const isUserLoggedIn = true
+// const temperature = 41
+
+// if(temperature === 4){
+//     console.log("Less than 50");
+    
+// }else{
+//     console.log("Temperature gretaer than 50");
+// }
+// console.log("Execute");
+
+
+
+// <,>,<=,>=,==,===,!==
+
+// const score =200
+// if (score > 100)
+// {
+//     let power = "fly"
+//     console.log(`User power : ${power}`);
+    
+// }
+// console.log(`User power : ${power}`);
+
+// const balance =1000
+// //  if (balance>500) console.log("test"),console.log("test2"); 
+
+//  if (balance < 500){
+//     console.log("less than 500");
+    
+//  }else if(balance <750){
+//     console.log("less than 750");
+//  }
+//  else if(balance <900){
+//     console.log("less than 900");
+//  }else{
+//     console.log("less than 1200");
+//  }
+
+const userLoggedIn = true
+const debitcard = true
+const loggedInFromGoogle = false
+const loggedInfromemail = true
+
+if(userLoggedIn && debitcard  && 2==3){
+    console.log("Allow to buy courses");
+    
+}
+ 
+ if (loggedInFromGoogle || loggedInfromemail ){
+    console.log("User Logged In");
+    
+ }
+
+
+
+
+
+
+
+
+
